6e1fb4de9d0cb2147828efbf60ca60c4f5a9c2a2
Gabriel Cardona <gjcardonam@gmail.com>
2025-06-21 22:17:55 -0500

    configs

---
+++ src/test/java/com/udea/restoct/RestoctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.restoct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestoctApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
